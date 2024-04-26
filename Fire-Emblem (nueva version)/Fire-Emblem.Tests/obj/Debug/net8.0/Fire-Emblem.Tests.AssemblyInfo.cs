50b11e2f894a83344dd1f04e6137171c85477885
CATALINA ORTEGA <catalinaortegacalderon@gmail.com>
2024-04-25 20:06:18 -0400

    limpiando código

--- Fire-Emblem (nueva version)/Fire-Emblem.Tests/obj/Debug/net8.0/Fire-Emblem.Tests.AssemblyInfo.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem.Tests/obj/Debug/net8.0/Fire-Emblem.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce1ec63ded0b7675fcc235a84f63f2bc3b32730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fce60ca784d899747e162e18c82806245913512")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Fire-Emblem (nueva version)/Fire-Emblem/GameElements/GameController.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem/GameElements/GameController.cs
@@ -11,7 +11,9 @@ public class GameController
     public bool roundIsTerminated = false;
     private Unit _currentAttackingUnit;
     private Unit _currentDefensiveUnit;
-    private int _numeroAtaque;
+    private int _numberOfThisRoundsCurrentAttack;
+    private int _firstPlayersCurrentUnitNumber;
+    private int _secondPlayersCurrentUnitNumber;
     
     // arreglos: hacer variables privadas (setter y getter)
     //  ver si dejo lineas largas o con enter
@@ -21,96 +23,73 @@ public class GameController
     // ver si dejo view como atributo o solo como param de funcion
     // IDEA: SOLO ATACKING PLAYER Y DEFENSE PLAYER, SACAR CURRENT PLAYER
 
-    public GameController(Player jugador1, Player jugador2)
+    public GameController(Player firstPlayer, Player secondPlayer)
     {
         this.currentAttacker = 0;
-        this.players[0] = jugador1;
-        this.players[1] = jugador2;
+        this.players[0] = firstPlayer;
+        this.players[1] = secondPlayer;
         this.gameIsTerminated = false;
     }
 
-    public string Attack(int numero_ataque, View view, int unidad1, int unidad2)
+    public string Attack(int numberOfCurrentAttack, View view, int firstPlayersCurrentUnitNumber, int secondPlayersCurrentUnitNumber)
     {
-        _numeroAtaque = numero_ataque;
-        string loosersName = "";
+        //PONER LOOSERS NAME COMO PARAMETRO
         if (this.gameIsTerminated || this.roundIsTerminated) return "";
-        // EL NUMERO DE ATAQUE PUEDE SER 1-2-3 DEPENDIENDO SI ES ATAQUE, CONTRAATAQUE O FOLLOWUP
+        _numberOfThisRoundsCurrentAttack = numberOfCurrentAttack;
+        this._firstPlayersCurrentUnitNumber = firstPlayersCurrentUnitNumber;
+        this._secondPlayersCurrentUnitNumber = secondPlayersCurrentUnitNumber;
+        string loosersName = "";
         int attackValue;
         if (currentAttacker == 0)
         {
-            _currentAttackingUnit = players[0].units[unidad1];
-            _currentDefensiveUnit = players[1].units[unidad2];
+            _currentAttackingUnit = players[0].units[_firstPlayersCurrentUnitNumber];
+            _currentDefensiveUnit = players[1].units[_secondPlayersCurrentUnitNumber];
         }
         else
         {
-            _currentAttackingUnit = players[1].units[unidad2];
-            _currentDefensiveUnit = players[0].units[unidad1];
+            _currentAttackingUnit = players[1].units[_secondPlayersCurrentUnitNumber];
+            _currentDefensiveUnit = players[0].units[_firstPlayersCurrentUnitNumber];
 
         }
 
-        if (numero_ataque == 1)
+        if (numberOfCurrentAttack == 1)
         {
-            ImprimirVentajas(view);
+            PrintAdvantages(view);
             ActivateAttackersUnitHabilities();
             ActivateDefensorsUnitHabilities();
             PrintHabilitiesInfo(view);
         }
-
-        attackValue = CalcularAtaque();
+        attackValue = CalculateAttack();
         view.WriteLine(_currentAttackingUnit.name + " ataca a " + _currentDefensiveUnit.name + " con " + attackValue + " de daño");
         if (currentAttacker == 0)
         {
-            if (attackValue >= players[1].units[unidad2].currentHp)
+            if (attackValue >= players[1].units[_secondPlayersCurrentUnitNumber].currentHp)
             {
-                //muere esta unidad
-                loosersName = players[1].units[unidad2].name;
+                loosersName = players[1].units[_secondPlayersCurrentUnitNumber].name;
                 this.roundIsTerminated = true;
-                // ERROR: LISTAS ES COPIA POR REFERENCIA
-                if (unidad2 == 0)
-                {
-                    players[1].units[0] = players[1].units[1];
-                    players[1].units[1] = players[1].units[2];
-                    players[1].units[2] = new Unit();
-                }
-                else if (unidad2 == 1)
-                {
-                    players[1].units[1] = players[1].units[2];
-                    players[1].units[2] = new Unit();
-                }
-                else
-                {
-                    players[1].units[2] = new Unit();
-                }
-
-                players[1].amountOfUnits = players[1].amountOfUnits - 1;
-                if (players[1].amountOfUnits == 0)
-                {
-                    this.gameIsTerminated = true;
-                    this.winner = 0;
-                    return loosersName;
-                }
+                EliminateLooserUnit(_secondPlayersCurrentUnitNumber);
                 return loosersName;
             }
             else
             {
-                players[1].units[unidad2].currentHp = players[1].units[unidad2].currentHp - attackValue;
+                SetDefendorsNewHp(_secondPlayersCurrentUnitNumber, attackValue);
+                return "";
             }
-            return "";
         }
         else
         {
-            if (attackValue >= players[0].units[unidad1].currentHp)
+            if (attackValue >= players[0].units[_firstPlayersCurrentUnitNumber].currentHp)
             {
-                loosersName = players[0].units[unidad1].name;
+                loosersName = players[0].units[_firstPlayersCurrentUnitNumber].name;
                 //muere esta unidad
                 this.roundIsTerminated = true;
-                if (unidad1 == 0)
+                if (_firstPlayersCurrentUnitNumber == 0)
                 {
                     players[0].units[0] = players[0].units[1];
                     players[0].units[1] = players[0].units[2];
                     players[0].units[2] = new Unit();
                 }
-                else if (unidad1 == 1)
+                else if (_firstPlayersCurrentUnitNumber == 1)
                 {
                     players[0].units[1] = players[0].units[2];
                     players[0].units[2] = new Unit();
@@ -132,10 +111,41 @@ public class GameController
             }
             else
             {
-                players[0].units[unidad1].currentHp = players[0].units[unidad1].currentHp - attackValue;
+                players[0].units[_firstPlayersCurrentUnitNumber].currentHp = players[0].units[_firstPlayersCurrentUnitNumber].currentHp - attackValue;
             }
 
             return "";
+        }
+    }
+
+    private void SetDefendorsNewHp(int secondPlayersCurrentUnitNumber, int attackValue)
+    {
+        players[1].units[secondPlayersCurrentUnitNumber].currentHp = players[1].units[secondPlayersCurrentUnitNumber].currentHp - attackValue;
+    }
+
+    private void EliminateLooserUnit(int secondPlayersCurrentUnitNumber)
+    {
+        if (secondPlayersCurrentUnitNumber == 0)
+        {
+            players[1].units[0] = players[1].units[1];
+            players[1].units[1] = players[1].units[2];
+            players[1].units[2] = new Unit();
+        }
+        else if (secondPlayersCurrentUnitNumber == 1)
+        {
+            players[1].units[1] = players[1].units[2];
+            players[1].units[2] = new Unit();
+        }
+        else
+        {
+            players[1].units[2] = new Unit();
+        }
+
+        players[1].amountOfUnits = players[1].amountOfUnits - 1;
+        if (players[1].amountOfUnits == 0)
+        {
+            this.gameIsTerminated = true;
+            this.winner = 0;
         }
     }
 
@@ -170,7 +180,7 @@ public class GameController
     }
     
 
-    public int CalcularAtaque()
+    public int CalculateAttack()
     {
         string arma_atac = _currentAttackingUnit.weapon;
         string arma_def = _currentDefensiveUnit.weapon;
@@ -178,13 +188,13 @@ public class GameController
         if (arma_atac == "Magic")
         {
             def_o_res_rival = _currentDefensiveUnit.res + _currentDefensiveUnit.activeBonus.res * _currentDefensiveUnit.activeBonusNeutralization.res + _currentDefensiveUnit.activePenalties.res *_currentDefensiveUnit.activePenaltiesNeutralization.res;
-            if (_numeroAtaque == 2) def_o_res_rival += _currentDefensiveUnit.activeBonus.resFirstAttack * _currentDefensiveUnit.activeBonusNeutralization.res + _currentDefensiveUnit.activePenalties.resFirstAttack *_currentDefensiveUnit.activePenaltiesNeutralization.res;
+            if (_numberOfThisRoundsCurrentAttack == 2) def_o_res_rival += _currentDefensiveUnit.activeBonus.resFirstAttack * _currentDefensiveUnit.activeBonusNeutralization.res + _currentDefensiveUnit.activePenalties.resFirstAttack *_currentDefensiveUnit.activePenaltiesNeutralization.res;
             Console.WriteLine("res rival" + def_o_res_rival);
         }
         else
         {
             def_o_res_rival = _currentDefensiveUnit.def + _currentDefensiveUnit.activeBonus.def * _currentDefensiveUnit.activeBonusNeutralization.def + _currentDefensiveUnit.activePenalties.def *_currentDefensiveUnit.activePenaltiesNeutralization.def;
-            if (_numeroAtaque == 2) def_o_res_rival += _currentDefensiveUnit.activeBonus.defFirstAttack * _currentDefensiveUnit.activeBonusNeutralization.def + _currentDefensiveUnit.activePenalties.defFirstAttack *_currentDefensiveUnit.activePenaltiesNeutralization.def;
+            if (_numberOfThisRoundsCurrentAttack == 2) def_o_res_rival += _currentDefensiveUnit.activeBonus.defFirstAttack * _currentDefensiveUnit.activeBonusNeutralization.def + _currentDefensiveUnit.activePenalties.defFirstAttack *_currentDefensiveUnit.activePenaltiesNeutralization.def;
             Console.WriteLine("def rival" + def_o_res_rival);
         }
         double wtb;
@@ -196,7 +206,7 @@ public class GameController
         }
         int atk_unidad = _currentAttackingUnit.attk + _currentAttackingUnit.activeBonus.attk * _currentAttackingUnit.activeBonusNeutralization.attk + _currentAttackingUnit.activePenalties.attk * _currentAttackingUnit.activePenaltiesNeutralization.attk;
         // revisar si pongo * neutralizador ataque o * neutralizador de atk first attack...
-        if (_numeroAtaque == 1)
+        if (_numberOfThisRoundsCurrentAttack == 1)
         {
             atk_unidad += _currentAttackingUnit.activeBonus.atkFirstAttack * _currentAttackingUnit.activeBonusNeutralization.attk +
                           _currentAttackingUnit.activePenalties.atkFirstAttack * _currentAttackingUnit.activePenaltiesNeutralization.attk;
@@ -204,7 +214,7 @@ public class GameController
             _currentAttackingUnit.gameLogs.amountOfAttacks++;
         }
         Console.WriteLine("ataque final antes de activar cambios por followup" + atk_unidad);
-        if (_numeroAtaque == 3)
+        if (_numberOfThisRoundsCurrentAttack == 3)
         {
             atk_unidad += _currentAttackingUnit.activeBonus.atkFollowup * _currentAttackingUnit.activeBonusNeutralization.attk
                 + _currentAttackingUnit.activePenalties.atkFollowup * _currentAttackingUnit.activePenaltiesNeutralization.attk;
@@ -217,7 +227,7 @@ public class GameController
         return Convert.ToInt32(Math.Truncate(atk_unidad * wtb - def_o_res_rival));
     }
 
-    public void ImprimirVentajas(View view)
+    public void PrintAdvantages(View view)
     {
         string arma_atac = _currentAttackingUnit.weapon;
         string arma_def = _currentDefensiveUnit.weapon;
