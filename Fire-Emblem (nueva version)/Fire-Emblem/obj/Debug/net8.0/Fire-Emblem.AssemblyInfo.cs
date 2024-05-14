a213879d84f0245bab79156d7e77e64509304d64
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-14 09:38:13 -0400

    avance

--- Fire-Emblem (nueva version)/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem/obj/Debug/net8.0/Fire-Emblem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a425c8d05fc889927cb7f66d035b58268764f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36a019bf473130a304ab020fc0e3a6cb3598abd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
