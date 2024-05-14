cfb08e0d097f943bb2daaa6bba10908ea015bd34
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-14 17:56:42 -0400

    dragon wall listo

--- Fire-Emblem (nueva version)/Fire-Emblem/SkillsManagement/Effects/DamageEffects/PercentualDamageReductionDeterminedByStatDifference.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem/SkillsManagement/Effects/DamageEffects/PercentualDamageReductionDeterminedByStatDifference.cs
@@ -10,17 +10,15 @@ public class PercentualDamageReductionDeterminedByStatDifference : Effect
     public PercentualDamageReductionDeterminedByStatDifference(string stat, int multiplicator) : base()
     {
         this.stat = stat;
-        this.multiplicator = this.multiplicator;
+        this.multiplicator = multiplicator;
     }
 
     public override void ApplyEffect(Unit myUnit, Unit opponentsUnit, bool attacking)
     { 
-        Console.WriteLine("aplicando efecto dragonwall");
         //poner el que queda no el reducido. ej: si se reduce en 10% el amount es 0.9
         double redutionPercentage = 1;
         if (this.stat == "Spd")
         {
-            Console.WriteLine("pase por spd");
             int myTotalSpd = 
                 myUnit.Spd + myUnit.ActiveBonus.Spd * myUnit.ActiveBonusNeutralization.Spd 
                            + myUnit.ActivePenalties.Spd * myUnit.ActivePenaltiesNeutralization.Spd;
@@ -31,20 +29,18 @@ public class PercentualDamageReductionDeterminedByStatDifference : Effect
         }
         else if (this.stat == "Res")
         {
-            int myTotalRes =
+            double myTotalRes =
                 myUnit.Res + myUnit.ActiveBonus.Res * myUnit.ActiveBonusNeutralization.Res
                            + myUnit.ActivePenalties.Res * myUnit.ActivePenaltiesNeutralization.Res;
-            int opponentsTotalRes =
+            double opponentsTotalRes =
                 opponentsUnit.Res + opponentsUnit.ActiveBonus.Res * opponentsUnit.ActiveBonusNeutralization.Res
                                   + opponentsUnit.ActivePenalties.Res * opponentsUnit.ActivePenaltiesNeutralization.Res;
             redutionPercentage =  1 - (((myTotalRes - opponentsTotalRes) * this.multiplicator)/100);
-            Console.WriteLine("pase por res");
         }
         if (redutionPercentage < 0.6)
         {
             redutionPercentage = 0.6;
         }
-        Console.WriteLine(redutionPercentage);
         myUnit.DamageEffects.PorcentualReduction = myUnit.DamageEffects.PorcentualReduction * redutionPercentage;
     }
     
