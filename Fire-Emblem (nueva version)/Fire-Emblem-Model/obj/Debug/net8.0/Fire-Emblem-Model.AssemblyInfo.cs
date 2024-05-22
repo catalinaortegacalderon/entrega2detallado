a70b6b1694e4ee32eedf630e41ed6181eb521fad
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-22 14:07:04 -0400

    empezando a avanzar con limpieza

--- Fire-Emblem (nueva version)/Fire-Emblem-Model/obj/Debug/net8.0/Fire-Emblem-Model.AssemblyInfo.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem-Model/obj/Debug/net8.0/Fire-Emblem-Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem-Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8140f399dcb791cb21fec26ee259e028f34485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9055c7d15ca6f77064302281b5a0db3fa3bc530")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem-Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem-Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
