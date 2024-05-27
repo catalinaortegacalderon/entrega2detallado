583cecad9a5619e9d4c82d5f22808ce0ff1d19c2
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-27 19:50:46 -0400

    avanzando con guard bearing

--- Fire-Emblem (nueva version)/Fire-Emblem-Model/SkillsManagement/Skills/HybridSkills/GuardBearing.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem-Model/SkillsManagement/Skills/HybridSkills/GuardBearing.cs
@@ -1,6 +1,18 @@
+using ConsoleApp1.DataTypes;
+
 namespace Fire_Emblem_Model;
 
 public class GuardBearing : Skill
 {
-    
+    public GuardBearing() : base()
+    {
+        this.Conditions = new Condition[3];
+        this.Conditions[0] = new AlwaysTrueCondition();
+        this.Conditions[1] = new AlwaysTrueCondition();
+        this.Conditions[2] = new MyUnitStartsCombatCondition();
+        this.Effects = new Effect[3];
+        this.Effects[0] = new ChangeOpponentsStatsInEffect(StatType.Spd,-4);
+        this.Effects[1] = new ChangeOpponentsStatsInEffect(StatType.Def,-4);
+        this.Effects[2] = new PercentualDamageReductionEffect(0.7, "First Attack");
+    }
 }
