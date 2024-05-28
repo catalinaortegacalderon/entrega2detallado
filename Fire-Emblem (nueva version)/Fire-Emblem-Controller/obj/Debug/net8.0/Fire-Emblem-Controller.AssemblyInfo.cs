3d52bcaa0e92087fe29cb604199c384c2b90bc8b
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-28 10:32:41 -0400

    avance view

--- Fire-Emblem (nueva version)/Fire-Emblem-Controller/obj/Debug/net8.0/Fire-Emblem-Controller.AssemblyInfo.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem-Controller/obj/Debug/net8.0/Fire-Emblem-Controller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b67d5ce89c0a7f51137e61ecce51257b6c2bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a71c2de94d758ce4fca89b0688a7989e3fb7ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
