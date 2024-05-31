2bc10a3d4bd083955ac144b4765648e803ed16ae
Catalina Ortega <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-05-31 09:42:56 -0400

    avance testcases

--- Fire-Emblem (nueva version)/Fire-Emblem-Model/SkillsManagement/Effects/DamageEffects/LunarBraceEffect.cs
+++ Fire-Emblem (nueva version)/Fire-Emblem-Model/SkillsManagement/Effects/DamageEffects/LunarBraceEffect.cs
@@ -8,7 +8,7 @@ public class LunarBraceEffect : Effect
     public override void ApplyEffect(Unit myUnit, Unit opponentsUnit)
     {
         int amount = opponentsUnit.Def + opponentsUnit.ActiveBonus.Def * opponentsUnit.ActiveBonusNeutralizator.Def
-                                       + opponentsUnit.ActivePenalties.Def * opponentsUnit.ActivePenalties.Def;
-        myUnit.DamageEffects.ExtraDamage = myUnit.DamageEffects.ExtraDamage + (int)Math.Truncate(0.3 * amount);
+                                       + opponentsUnit.ActivePenalties.Def * opponentsUnit.ActivePenaltiesNeutralizator.Def;
+        myUnit.DamageEffects.ExtraDamage += (int)Math.Truncate(0.3 * amount);
     }
 }
